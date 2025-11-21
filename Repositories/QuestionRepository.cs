10706840ec60c37bc86290aa209cc372fff31856
MRtim <timatosalikaze@gmail.com>
2025-11-21 22:17:33 +0500

    изменил логирование

--- Repositories/QuestionRepository.cs
+++ Repositories/QuestionRepository.cs
@@ -1,5 +1,6 @@
 ﻿using AutoMapper;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
 using TestingPlatform.Application.Dtos;
 using TestingPlatform.Application.Interfaces;
 using TestingPlatform.Domain.Models;
@@ -8,16 +9,21 @@ using TestingPlatform.Infrastructure.Exceptions;
 
 namespace TestingPlatform.Infrastructure.Repositories;
 
-public class QuestionRepository(AppDbContext appDbContext, IMapper mapper) : IQuestionRepository
+public class QuestionRepository(AppDbContext appDbContext, IMapper mapper, ILogger<QuestionRepository> logger) : IQuestionRepository
 {
     public async Task<IEnumerable<QuestionDto>> GetAllAsync()
     {
+        logger.LogInformation("Получен запрос на получение всех вопросов");
         var questions = await appDbContext.Question.AsNoTracking().ToListAsync();
+
+        logger.LogInformation("Из базы получено {Count} вопросов", questions.Count);
         return mapper.Map<IEnumerable<QuestionDto>>(questions);
     }
 
     public async Task<QuestionDto> GetByIdAsync(int id)
     {
+        logger.LogInformation("Запрос на получение вопроса Id={QuestionId}", id);
+
         var question = await appDbContext.Question
             .AsNoTracking()
             .FirstOrDefaultAsync(question => question.Id == id);
@@ -27,21 +33,26 @@ public class QuestionRepository(AppDbContext appDbContext, IMapper mapper) : IQu
             throw new EntityNotFoundException("Вопрос не найден.");
         }
 
+        logger.LogInformation("Вопрос Id={QuestionId} успешно найден", id);
         return mapper.Map<QuestionDto>(question);
     }
 
     public async Task<int> CreateAsync(QuestionDto questionDto)
     {
+        logger.LogInformation("Создание нового вопроса для теста TestId={TestId}", questionDto.TestId);
         var question = mapper.Map<Question>(questionDto);
 
         var questionId = await appDbContext.AddAsync(question);
         await appDbContext.SaveChangesAsync();
 
+        logger.LogInformation("Вопрос успешно создан с Id={QuestionId}", questionId.Entity.Id);
         return questionId.Entity.Id;
     }
 
     public async Task UpdateAsync(QuestionDto questionDto)
     {
+        logger.LogInformation("Обновление вопроса Id={QuestionId}", questionDto.Id);
+
         var question = await appDbContext.Question.FirstOrDefaultAsync(question => question.Id == questionDto.Id);
 
         if (question == null)
@@ -57,10 +68,14 @@ public class QuestionRepository(AppDbContext appDbContext, IMapper mapper) : IQu
         question.TestId = questionDto.TestId;
 
         await appDbContext.SaveChangesAsync();
+
+        logger.LogInformation("Вопрос Id={QuestionId} успешно обновлён", questionDto.Id);
     }
 
     public async Task DeleteAsync(int id)
     {
+        logger.LogInformation("Удаление вопроса Id={QuestionId}", id);
+
         var question = await appDbContext.Question.FirstOrDefaultAsync(question => question.Id == id);
 
         if (question == null)
@@ -70,6 +85,8 @@ public class QuestionRepository(AppDbContext appDbContext, IMapper mapper) : IQu
 
         appDbContext.Question.Remove(question);
         await appDbContext.SaveChangesAsync();
+
+        logger.LogInformation("Вопрос Id={QuestionId} успешно удалён", id);
     }
 }
 
